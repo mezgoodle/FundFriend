eadd064f768ac2388e11164f5c0c5c8836283070
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-01 15:43:34 +0200

    Create auth app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core.apps.CoreConfig",
     "core.user.apps.UserConfig",
+    "core.auth.apps.AuthConfig",
     "rest_framework",
     "rest_framework_simplejwt",
 ]
