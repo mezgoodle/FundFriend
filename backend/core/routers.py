f12d76f1bb22a9e227c5ed4ce8e28d3534a5a40e
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-01 15:55:47 +0200

    Add register viewset

--- backend/core/routers.py
+++ backend/core/routers.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
+from core.auth.viewsets import RegisterViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"auth/register", RegisterViewSet, basename="register")
 
 urlpatterns = [*router.urls]
