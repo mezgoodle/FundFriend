9463780ea6161b2c21e8115ec17c078e4b830c1c
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-08 16:35:39 +0200

    Rewrite method to_representation

--- backend/core/bank/serializers.py
+++ backend/core/bank/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import ValidationError
 from core.abstract.serializers import AbstractSerializer
 from core.bank.models import Bank
 from core.user.models import User
+from core.user.serializers import UserSerializer
 
 
 class BankSerializer(AbstractSerializer):
@@ -15,6 +16,12 @@ class BankSerializer(AbstractSerializer):
         if self.context["request"].user != value:
             raise ValidationError("You can't edit this bank")
         return value
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        author = User.objects.get_object_by_public_id(rep["author"])
+        rep["author"] = UserSerializer(author).data
+        return rep
 
     class Meta:
         model = Bank
