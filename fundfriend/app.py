e00ffba87db842f5e888fcfc484e15d59914b010
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-10 12:56:41 +0200

    Fix

--- fundfriend/app.py
+++ fundfriend/app.py
@@ -81,7 +81,6 @@ APP.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
     try:
-        # web.run_app(APP, host="localhost", port=CONFIG.PORT)
-        web.run_app(APP)
+        web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
         raise error
