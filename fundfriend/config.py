b868f9adec6b047563cb5f587bedd40d3f84553c
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-11 10:07:20 +0200

    Try with the 8000 port

--- fundfriend/config.py
+++ fundfriend/config.py
@@ -4,9 +4,10 @@
 
 import os
 
-class DefaultConfig:
-    """ Bot Configuration """
 
-    PORT = 3978
+class DefaultConfig:
+    """Bot Configuration"""
+
+    PORT = 8000
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
