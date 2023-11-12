d5d277d42439c65423f0df90f8a10bce15c4be38
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-11-12 11:24:33 +0200

    Install ruff

--- fundfriend/config.py
+++ fundfriend/config.py
@@ -9,7 +9,9 @@ class DefaultConfig:
     """Bot Configuration"""
 
     PORT = os.environ.get("PORT", 8000)
-    APP_ID = os.environ.get("MicrosoftAppId", "547df17e-07a6-4f8a-a745-a8bd3bf65a56")
+    APP_ID = os.environ.get(
+        "MicrosoftAppId", "547df17e-07a6-4f8a-a745-a8bd3bf65a56"
+    )
     APP_PASSWORD = os.environ.get(
         "MicrosoftAppPassword", "U6g8Q~SV-3F1RtCwoiKrq4B14kkM_fQSeyUrLaUS"
     )
