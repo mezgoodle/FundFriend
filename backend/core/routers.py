44f7d742a6359e0bfdddc9132d2a6f02ea02240a
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-12-07 13:55:12 +0200

    Install nested-drf-routes

--- backend/core/routers.py
+++ backend/core/routers.py
@@ -1,4 +1,4 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from core.auth.viewsets import LoginViewSet, RefreshViewSet, RegisterViewSet
 from core.bank.viewsets import BankViewSet
@@ -8,9 +8,12 @@ from core.user.viewsets import UserViewSet
 router = routers.SimpleRouter()
 router.register(r"users", UserViewSet, basename="user")
 router.register(r"banks", BankViewSet, basename="bank")
-router.register(r"documents", DocumentViewSet, basename="document")
 router.register(r"auth/register", RegisterViewSet, basename="register")
 router.register(r"auth/login", LoginViewSet, basename="login")
 router.register(r"auth/refresh", RefreshViewSet, basename="auth-refresh")
 
-urlpatterns = [*router.urls]
+banks_router = routers.NestedSimpleRouter(router, r"banks", lookup="bank")
+banks_router.register(r"documents", DocumentViewSet, basename="bank-documents")
+
+
+urlpatterns = [*router.urls, *banks_router.urls]
