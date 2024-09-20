9f7e71c0b3dcb24a9bf3a2d099437d7456db2b17
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-09-20 15:25:57 +0300

    Add example responses

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,14 +19,16 @@ def get_db():
 
 
 @app.get("/")
-def root():
+def root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.post(
     "/users/", response_model=schemas.User, status_code=status.HTTP_201_CREATED
 )
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(
+    user: schemas.UserCreate, db: Session = Depends(get_db)
+) -> schemas.User:
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(
@@ -39,13 +41,15 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @app.get("/users/", response_model=list[schemas.User])
 async def read_users(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
+) -> list[schemas.User]:
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
+async def read_user(
+    user_id: int, db: Session = Depends(get_db)
+) -> schemas.User:
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -59,13 +63,13 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
 )
 async def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
+) -> schemas.Item:
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
 @app.get("/items/", response_model=list[schemas.Item])
 async def read_items(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
+) -> list[schemas.Item]:
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
