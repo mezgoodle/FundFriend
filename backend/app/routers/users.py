f93f89f497a852f3b12410be39fe852981e8a8bb
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-10-06 20:27:07 +0300

    Create package schemas

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,18 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from .. import crud, schemas
+from .. import crud
 from ..dependencies import get_db
+from ..schemas.user import User, UserCreate
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post(
-    "/", response_model=schemas.User, status_code=status.HTTP_201_CREATED
-)
-async def create_user(
-    user: schemas.UserCreate, db: Session = Depends(get_db)
-) -> schemas.User:
+@router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> User:
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(
@@ -24,24 +21,22 @@ async def create_user(
 
 @router.get(
     "/",
-    response_model=list[schemas.User],
+    response_model=list[User],
     status_code=status.HTTP_200_OK,
 )
 async def read_users(
     skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-) -> list[schemas.User]:
+) -> list[User]:
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @router.get(
     "/{user_id}",
-    response_model=schemas.User,
+    response_model=User,
     status_code=status.HTTP_200_OK,
 )
-async def read_user(
-    user_id: int, db: Session = Depends(get_db)
-) -> schemas.User:
+async def read_user(user_id: int, db: Session = Depends(get_db)) -> User:
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
