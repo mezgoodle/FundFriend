272bd4955def9dd1a09fe8257461332cf114c00a
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-10-07 22:31:52 +0300

    Update schemas

--- backend/app/schemas/chat.py
+++ backend/app/schemas/chat.py
@@ -1,28 +1,22 @@
-from typing import List, Optional
-
 from pydantic import BaseModel
 
 
-# Base модель для спільних полів
 class ChatBase(BaseModel):
-    message: str
+    title: str
 
 
-# Модель для створення чату
 class ChatCreate(ChatBase):
     pass
 
 
-# Модель для оновлення чату
 class ChatUpdate(ChatBase):
     pass
 
 
-# Модель для відображення чату (включаючи пов'язані повідомлення)
 class ChatOut(ChatBase):
     id: int
     owner_id: int
-    messages: List[dict] = []
+    messages: list[dict] = []
 
     class Config:
         orm_mode = True
--- backend/app/schemas/message.py
+++ backend/app/schemas/message.py
@@ -3,25 +3,21 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-# Base модель для спільних полів
 class MessageBase(BaseModel):
-    message: str
-
-
-# Модель для створення повідомлення
-class MessageCreate(MessageBase):
+    text: str
     chat_id: int
 
 
-# Модель для оновлення повідомлення
+class MessageCreate(MessageBase):
+    pass
+
+
 class MessageUpdate(MessageBase):
-    chat_id: Optional[int] = None
+    text: Optional[str] = None
 
 
-# Модель для відображення повідомлення
 class MessageOut(MessageBase):
     id: int
-    chat_id: int
     owner_id: int
 
     class Config:
