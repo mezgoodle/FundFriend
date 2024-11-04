804b9f909f76489d1eb5b737a977e4778970b3ff
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-11-04 22:38:03 +0200

    Remove unused imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
-import logging
-import time
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 
