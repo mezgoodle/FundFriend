2465226fd159d6fe54d63f296a25a2352f851a71
Maksym Zavalniuk <mezgoodle@gmail.com>
2024-11-04 22:35:31 +0200

    Add loguru

--- backend/app/middlewares/logging.py
+++ backend/app/middlewares/logging.py
@@ -1,29 +1,43 @@
-import logging
+import json
 import time
 
 from fastapi import Request
+from loguru import logger
 
-logger = logging.getLogger()
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.StreamHandler()],
+logger.add(
+    "logs/app.log",
+    format="{time} | {level} | {message}",
+    level="INFO",
+    rotation="10 MB",
+    serialize=True,
 )
 
 
 async def log_middleware(request: Request, call_next):
+    start_time = time.time()
+
+    # Extract details from the request
+    request_body = await request.body()
+    request_data = {
+        "method": request.method,
+        "url": str(request.url),
+        "query_params": dict(request.query_params),
+        "body": request_body.decode("utf-8") if request_body else None,
+    }
+
+    response = await call_next(request)
+
+    duration = time.time() - start_time
+
     log_dict = {
         "method": request.method,
-        "path": request.url.path,
-        "status": 0,
-        "duration": 0,
+        "url": str(request.url),
+        "status_code": response.status_code,
+        "duration": duration,
+        "request": request_data,
+        "response_status_code": response.status_code,
     }
-    start_time = time.time()
-    response = await call_next(request)
-    log_dict["status"] = response.status_code
-    log_dict["duration"] = time.time() - start_time
 
-    logger.info(log_dict)
+    logger.info(json.dumps(log_dict, indent=2))
 
     return response
